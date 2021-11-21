a5c8b13c4edec7bc8eb901554f76b7de570932e7
GwnDaan <thadutchgameryt@gmail.com>
2021-11-21 13:00:00 +0100

    Implemented extended transport protocol (untested)

--- pyisolib/__init__.py
+++ pyisolib/__init__.py
@@ -3,3 +3,4 @@ from .working_set import WorkingSet
 from .vt_objects.graphics_objects import GraphicsObject
 from .vt_objects.listed_items import *
 from .vt_objects.top_level_objects import *
+from .vt_objects.line_objects import *
--- pyisolib/working_set.py
+++ pyisolib/working_set.py
@@ -1,4 +1,7 @@
+import math
+import time
 import j1939
+from pyisolib.extended_tp import ExtendedTP
 
 from . import functions
 from .pgns import PGNS
@@ -26,8 +29,8 @@ class WorkingSet:
             The controller application class 
         """
         self.ca = ca
-        self.state = WorkingSet.State.NONE
-        self._object_pool = ObjectPool()
+        self.__state = WorkingSet.State.NONE
+        self.__object_pool = ObjectPool()
         
     def start(self):
         """Start the working set. The controller application must be started and the object pool set!
@@ -35,28 +38,28 @@ class WorkingSet:
         if self.ca.state != j1939.ControllerApplication.State.NORMAL:
             raise RuntimeError("ControllerApplication must be started before initializing WorkingSet!")
         
-        if not self._object_pool.is_ready():
+        if not self.__object_pool.is_ready():
             raise RuntimeError("The object pool is not yet ready, make sure you added objects!")
         
-        self.ca.subscribe(self._on_message)
-        self.ca.add_timer(1, self._tick, cookie=self.ca) # Send the maintenance message every second
-        self.state = WorkingSet.State.AWAITING_VT_STATUS
+        self.ca.subscribe(self.__on_message)
+        self.ca.add_timer(1, self.__tick, cookie=self.ca) # Send the maintenance message every second
+        self.__state = WorkingSet.State.AWAITING_VT_STATUS
         
     def add_object_to_pool(self, object):
-        if self.state >= WorkingSet.State.UPLOADING_POOL:
+        if self.__state >= WorkingSet.State.UPLOADING_POOL:
             # TODO: implement realtime editing of pool
             raise NotImplementedError()
         
-        self._object_pool.add_object(object)
+        self.__object_pool.add_object(object)
     
-    def _next_state(self):
+    def __next_state(self):
         """Set current state to next"""
-        if self.state + 1 > WorkingSet.State.NORMAL:
+        if self.__state + 1 > WorkingSet.State.NORMAL:
             raise RuntimeError("Next state not possible as it is not defined.")
         else:
-            self.state += 1
+            self.__state += 1
     
-    def _on_message(self, priority, pgn, sa, timestamp, data):
+    def __on_message(self, priority, pgn, sa, timestamp, data):
         """Used to receive message from the VT.
         """
         if pgn == PGNS.VT_TO_ECU:
@@ -65,22 +68,22 @@ class WorkingSet:
             function = data[0]
             print("Received from VT: ", function)
             if function == functions.Status.VT_STATUS:
-                if self.state == WorkingSet.State.AWAITING_VT_STATUS:
-                    self._next_state()
+                if self.__state == WorkingSet.State.AWAITING_VT_STATUS:
+                    self.__next_state()
                     
             elif function == functions.TechinalData.GET_HARDWARE:
-                assert self.state == WorkingSet.State.AWAIT_HARDWARE
+                assert self.__state == WorkingSet.State.AWAIT_HARDWARE
                 self.x_pixels = int.from_bytes(data[4:6], 'little')
                 self.y_pixels = int.from_bytes(data[6:8], 'little')
                 
                 # We completed hardware state, next is memory
-                self._next_state()
+                self.__next_state()
                 self.send(PGNS.ECU_TO_VT, 7, 
                         # Data follows below: TODO: remove static memory of 200000 and calculate it
                         functions.TechinalData.GET_MEMORY, 0xFF, (200000).to_bytes(4, 'little'))
             
             elif function == functions.TechinalData.GET_MEMORY:
-                assert self.state == WorkingSet.State.AWAIT_MEMORY
+                assert self.__state == WorkingSet.State.AWAIT_MEMORY
                 self.vt_version = data[1]
                 memory_error = data[2]
                 
@@ -88,7 +91,7 @@ class WorkingSet:
                 if memory_error == 1:
                     raise RuntimeError("Not enough memory available to hold our object pool!")
                 else:
-                    self._next_state()
+                    self.__next_state()
             
             elif function == functions.TransferObjectPool.END_OF_POOL:
                 error_code = data[1]
@@ -99,50 +102,55 @@ class WorkingSet:
                     object_pool_error_code = data[6]
                     raise RuntimeError(f"END_OF_POOL_ERROR: error {error_code}, parent_faulty_object {parent_faulty_object}, faulty_object {faulty_object}, object_pool_error {object_pool_error_code}")
                 else:
-                    self._next_state()
+                    self.__next_state()
                     
-    def _tick(self, _):
+    def __tick(self, _):
         """Check if we need to perform any actions"""
         
-        print("state", self.state)
+        print("state", self.__state)
 
         # Announce the current working set as master if state is set
-        if self.state == WorkingSet.State.ANNOUNCING_WORKING_MASTER:
+        if self.__state == WorkingSet.State.ANNOUNCING_WORKING_MASTER:
             self.send(PGNS.WORKING_SET_MASTER, 7,
                       #Data follows below:
                       1, completer=0)
-            self._next_state()
+            self.__next_state()
         
         # Send the maintenance message IFF the state is the init maintenance state or above
-        elif self.state >= WorkingSet.State.INIT_MAINTENANCE:
-            initializing = self.state == WorkingSet.State.INIT_MAINTENANCE
+        elif self.__state >= WorkingSet.State.INIT_MAINTENANCE:
+            initializing = self.__state == WorkingSet.State.INIT_MAINTENANCE
             self.send(PGNS.ECU_TO_VT, 7, 
                     # Data follows below:
                     functions.Status.MAINTENANCE, 1 if initializing else 0)
             
             # Get hardware and set to next state if we are currently initializing.
             if initializing:
-                self._next_state()
+                self.__next_state()
                 print("Sending hardware request")
                 self.send(PGNS.ECU_TO_VT, 7, 
                         # Data follows below:
                         functions.TechinalData.GET_HARDWARE)
         
         # # Upload the object pool IFF the state is set
-        if self.state == WorkingSet.State.UPLOADING_POOL:
+        if self.__state == WorkingSet.State.UPLOADING_POOL:
+            print("---------------")
             print("Getting pool data")
-            body = self._object_pool.get_data()
+            body = self.__object_pool.get_data()
             print("Got data... sending")
             self.send(PGNS.ECU_TO_VT, 7, 
                       # Data follows below:
                       functions.TransferObjectPool.TRANSFER, body)
             print("Data sent!")
-            
+            print("---------------")
+
             # Successfully uploaded the complete pool, tell the vt it is the end
-            self.send(PGNS.ECU_TO_VT, 7, functions.TransferObjectPool.END_OF_POOL)
-            self._next_state()
+            self.ca.add_timer(3, self.__send_end_of_pool)
+            self.__next_state()
         
         return True
+    
+    def __send_end_of_pool(self, _):
+        self.send(PGNS.ECU_TO_VT, 7, functions.TransferObjectPool.END_OF_POOL)
                     
     def send(self, pgn, priority, *args, length=8, completer=0xFF):
         """Completes and sends the args as pgn.
@@ -156,19 +164,31 @@ class WorkingSet:
         :param int completer:
             The value which will be appended to the data to get the length.
         """
+        # Assert that we can actually send data
+        if self.__state <= WorkingSet.State.AWAITING_VT_STATUS:
+            raise RuntimeError("WorkingSet not yet ready to send data", self.__state)
+
         data = bytes(0)
         for element in args:
             data += element if isinstance(element, bytes) else bytes([element])
+       
+       # We only need to complete if len of data is less than 8.
+       # The transport protocol and extended transport protocol will handle it themselves
+        if len(data) < 8:
+            data += bytes((completer for _ in range(length - len(data))))
 
-        data += bytes((completer for _ in range(length - len(data))))
-        assert len(data) == length
-        
         # Disasembling the pgn
         data_page = (pgn >> 16) & 0x01
         pdu_format = (pgn >> 8) & 0xFF
         pdu_specific = pgn & 0xFF
         
-        # Send the actual pgn
-        success = self.ca.send_pgn(data_page, pdu_format, pdu_specific, priority, data)
-        if not success:
-            raise RuntimeError("Sending pgn failed!")
+        # Send the actual pgn, if length is 1786 we use ETP otherwise we use j1939 python module to send it
+        if len(data) >= 1786:
+            print("ETP send: ", len(data))
+            etp = ExtendedTP(self.ca, priority, pdu_specific)
+            etp.send(data_page, pdu_format, pdu_specific, data)
+        else:
+            print("Normal send: ", len(data))
+            success = self.ca.send_pgn(data_page, pdu_format, pdu_specific, priority, bytearray(data))
+            if not success:
+                raise RuntimeError("Sending pgn failed!")
