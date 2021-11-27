ea33cb27ed2f35290e794d295c75aca63383a1f2
GwnDaan <thadutchgameryt@gmail.com>
2021-11-27 13:17:35 +0100

    Added rectangle object

--- pyisolib/vt_objects/attribute_objects.py
+++ pyisolib/vt_objects/attribute_objects.py
@@ -3,7 +3,6 @@ from dataclasses import dataclass, field
 from .object_utils import object_to_bytes
 from .abstract_object import DataObject
 
-
 @dataclass
 class FontAttributes(DataObject):
     _TYPE = 23 # Byte 3
@@ -13,6 +12,7 @@ class FontAttributes(DataObject):
     font_size: int # Byte 5
     font_type: int # Byte 6
     font_style: int # Byte 7
+    
     macros: list = field(default_factory=list) # Number of macros = byte 8, repeated with starting byte 9
     
     # Overrides from DataObject
@@ -29,10 +29,28 @@ class LineAttribute(DataObject):
     line_color: int # Byte 4
     line_width: int # Byte 5
     line_art: int # Byte 6-7
-    macros:list = field(default_factory=list) # Number of macros = byte 8, repeated with starting byte 9
+    
+    macros: list = field(default_factory=list) # Number of macros = byte 8, repeated with starting byte 9
     
     # Overrides from DataObject
     def get_data(self):
         return object_to_bytes([self.object_id, self._TYPE, self.line_color, self.line_width, self.line_art, len(self.macros), self.macros],
+                               # The following are the byte_length of each data value
+                               2, 1, 1, 1, 2, 1, 2)
+
+@dataclass
+class FillAttribute(DataObject):
+    _TYPE = 25 # Byte 3
+    
+    object_id: int # Byte 1-2
+    fill_type: int # Byte 4
+    fill_color: int # Byte 5
+    fill_pattern: int # Byte 6-7
+    
+    macros: list = field(default_factory=list) # Number of macros = byte 8, repeated with starting byte 9
+    
+    # Overrides from DataObject
+    def get_data(self):
+        return object_to_bytes([self.object_id, self._TYPE, self.fill_type, self.fill_color, self.fill_pattern, len(self.macros), self.macros],
                                # The following are the byte_length of each data value
                                2, 1, 1, 1, 2, 1, 2)
--- pyisolib/vt_objects/shape_objects.py
+++ pyisolib/vt_objects/shape_objects.py
@@ -19,3 +19,21 @@ class LineObject(DataObject):
         return object_to_bytes([self.object_id, self._TYPE, self.line_attributes, self.width, self.height, self.line_direction, len(self.macros), self.macros],
                                # The following are the byte_length of each data value
                                2, 1, 2, 2, 2, 1, 1, 2)
+
+@dataclass
+class RectangleObject(DataObject):
+    _TYPE = 14 # Byte 3
+    
+    object_id: int # BYte 1-2
+    line_attributes: int # Byte 4-5
+    width: int # Byte 6-7
+    height: int # Byte 8-9
+    line_suppresion: int # Byte 10
+    fill_attributes: int # Byte 11-12
+    macros: list = field(default_factory=list) # Number of macros = byte 13, repeated with starting byte 14
+    
+    # Overrides from DataObject
+    def get_data(self):
+        return object_to_bytes([self.object_id, self._TYPE, self.line_attributes, self.width, self.height, self.line_suppresion, self.fill_attributes, len(self.macros), self.macros],
+                               # The following are the byte_length of each data value
+                               2, 1, 2, 2, 2, 1, 2, 1, 2)
