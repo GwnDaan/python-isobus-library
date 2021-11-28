6ab2e41d27143064e043e2c71c8db7712301fa55
GwnDaan <thadutchgameryt@gmail.com>
2021-11-28 14:20:55 +0100

    Added float test

--- pyisolib/packet_utils.py
+++ pyisolib/packet_utils.py
@@ -28,18 +28,24 @@ def object_to_bytes(data: list, *byte_length) -> bytes:
         elif isinstance(value, SignedInt):
             # Make sure that we have the int byte_length type
             if not isinstance(length, int):
-                raise RuntimeError("We got an invalid byte_length type for 'int' data", length)
+                raise RuntimeError("We got an invalid byte_length type for 'signedint' data", length)
             result += value.get_data(length)
+
+        elif isinstance(value, float):
+            # Make sure that we have the int byte_length type
+            if length != 4:
+                raise RuntimeError("We got type float but byte_length is not '4'", length)
+            result += IEEE754(value)
         
         elif isinstance(value, bool):
-            if (length != 1):
+            if length != 1:
                 raise RuntimeError("We got type bool but byte_length is not '1'", length)
             result += bytes(1) if value else bytes(0)
         
         elif isinstance(value, str):
             # Make sure that we have the int byte_length type
             if not isinstance(length, int):
-                raise RuntimeError("We got an invalid byte_length type for 'int' data", length)
+                raise RuntimeError("We got an invalid byte_length type for 'str' data", length)
             
             #TODO figure out if this is the correct way of displaying string
             encoded = value.encode('iso-8859-1')
@@ -78,3 +84,58 @@ class SignedInt(DataObject):
     # Overrides from DataObject
     def get_data(self, length) -> bytes:
         return self.value.to_bytes(length, 'little', signed=True)
+
+#TODO: below is copied and therefore only for testing
+# Function for converting decimal to binary
+def float_bin(my_number, places = 3):
+    my_whole, my_dec = str(my_number).split(".")
+    my_whole = int(my_whole)
+    res = (str(bin(my_whole))+".").replace('0b','')
+ 
+    for x in range(places):
+        my_dec = str('0.')+str(my_dec)
+        temp = '%1.20f' %(float(my_dec)*2)
+        my_whole, my_dec = temp.split(".")
+        res += my_whole
+    return res
+ 
+def IEEE754(n):
+    # identifying whether the number
+    # is positive or negative
+    sign = 0
+    if n < 0 :
+        sign = 1
+        n = n * (-1)
+    p = 30
+    # convert float to binary
+    dec = float_bin (n, places = p)
+ 
+    dotPlace = dec.find('.')
+    onePlace = dec.find('1')
+    # finding the mantissa
+    if onePlace > dotPlace:
+        dec = dec.replace(".","")
+        onePlace -= 1
+        dotPlace -= 1
+    elif onePlace < dotPlace:
+        dec = dec.replace(".","")
+        dotPlace -= 1
+    mantissa = dec[onePlace+1:]
+ 
+    # calculating the exponent(E)
+    exponent = dotPlace - onePlace
+    exponent_bits = exponent + 127
+ 
+    # converting the exponent from
+    # decimal to binary
+    exponent_bits = bin(exponent_bits).replace("0b",'')
+ 
+    mantissa = mantissa[0:23]
+ 
+    # the IEEE754 notation in binary    
+    final = str(sign) + exponent_bits.zfill(8) + mantissa
+ 
+    # convert the binary to hexadecimal
+    # hstr = '0x%0*X' %((len(final) + 3) // 4, int(final, 2))
+    # return (hstr, final)
+    return int(final, 2).to_bytes(len(final) // 8, byteorder='big')
